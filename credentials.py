b8244e1296ba322c63e9636fac14cbb18c0d78fc
Hezron-tech <kiprophez@gmail.com>
2022-02-20 01:19:25 +0300

    creating test display credentials

--- credentials.py
+++ credentials.py
@@ -1,9 +1,7 @@
 
-
-
 class Credential:
 
-    Credential_list=[]
+    credential_list=[]
 
     def __init__(self,account_name,username,account_password):
         self.account_name=account_name
@@ -12,14 +10,54 @@ class Credential:
         
 
     def save_credential(self):
-        Credential.Credential_list.append(self)
+        Credential.credential_list.append(self)
+
+
+
+    def delete_credentials(self):
+        '''
+        delete saved credentials in the credentials list
+        '''
+        Credential.credentials_list.remove(self) 
+
+
+
+    def create_credentials(account_name,email,passcode):
+        new_credentials=Credential.credential_list(account_name,email,passcode) 
+        return new_credentials  
+
+
+
+
+    @classmethod
+    def verify_credential(cls,account_name):
+         """
+           method used to verify credentials
+         """
+         for credential in Credential.Credential_list:
+             if(credential.account_name==account_name):
+                 return True
+                 return False    
+
+        
+             
+
+
+    @classmethod
+    def find_account(cls,account_name):
+         for credential in cls.Credential.Credential_list:
+             if(credential.account_name==account_name):
+                 return credential
+
+
+    
+
 
     
 
 
 
-    
-            
+       
 
 
 
--- credentials_test.py
+++ credentials_test.py
@@ -1,43 +1,69 @@
+import pyperclip
 import unittest
 from credentials import Credential
 
 class TestCredentials(unittest.TestCase):
+
+
     def setUp(self):
         '''
         method to run before each test
         '''
         self.new_credential=Credential("Gmail","Hezzy","geek")
+
+        
     def test_init(self):
         self.assertEqual(self.new_credential.account_name, "Gmail")
         self.assertEqual(self.new_credential.username,"Hezzy")
         self.assertEqual(self.new_credential.account_password, "geek")
         
 
+    
+
+    def test_save_credential(self):
+        self.new_credential.save_credential()
+        self.assertEqual( len(Credential.credential_list), 1) 
+
+
+
     def tearDown(self):
         '''
         clean up after each test to prevent errors
         '''
-        Credential.credentials_list = []   
+        Credential.credential_list=[]    
+          
 
-    def save_multiple_credential(self):
+
+
+    def test_save_multiple_credentials(self):
         self.new_credential.save_credential() 
         test_credential=Credential("FB","glovu","1234") 
         test_credential.save_credential()
-        self.assertEqual(len(Credential.credential_list),2)
+        self.assertEqual( len(Credential.credential_list), 2)
 
 
-        
-
-    def delete_credentials(self):
+    def test_confirm_credentials_exists(self):
         '''
-        delete saved credentials in the credentials list
+        confirm that credentials actually exists
         '''
-        Credential.credentials_list.remove(self)   
+        self.new_credentials.save_credentials()
+        test_credentials = Credential("Twitter", "trythis","passcode")
+        test_credentials.save_credentials()
+        credentials_exists = Credential.credentials_exists("Twitter")
+        self.assertTrue(credentials_exists)
+##displaying the credentials
+    def test_display_credentials(self):
+        '''
+        test if all credentials can be displayed
+        '''
+        self.assertEqual(Credential.display_credentials(), Credential.credentials_list)
+        ##copy    
 
 
 
-           
 
+
+       
 
 if __name__ == '__main__':
     unittest.main()
