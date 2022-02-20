6924acdea1976f6e9a98e49782721d2ce5d27711
Hezron-tech <kiprophez@gmail.com>
2022-02-20 10:38:10 +0300

    adding method that delete user

--- run.py
+++ run.py
@@ -2,7 +2,7 @@ from users import User
 from credentials import Credential
 
 
-def save_user(username,password):
+def create_user(username,password):
     '''
 
     function to add new user
@@ -10,13 +10,30 @@ def save_user(username,password):
     new_user=User(username,password)
     return new_user
 
-def delete_user(username):
-    User.delete_user(username)
+def save_user(self):
+        User.user_list.append(self)
+
+def display_user(cls):
+        return cls.user_list        
 
 
 
 
 
+
+
+def login_user(username,password):
+    '''
+    a fumction that checks if the users already exist 
+    '''
+    checked_user = Credential.verify_user(username,password)
+    return checked_user
+
+
+def delete_user(self):
+        User.user_list.remove(self)   
+        
+         
 
 def create_new_credential(account_name,email,passcode):
     '''
